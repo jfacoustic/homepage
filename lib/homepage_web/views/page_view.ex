37250b43e73d9f98cc1e6b174d62bc8c3017cdbf
Joshua Mathews <joshfeltonm@gmail.com>
2022-09-02 20:21:04 -0600

    styling code in blog posts

--- lib/homepage_web/views/page_view.ex
+++ lib/homepage_web/views/page_view.ex
@@ -2,6 +2,11 @@ defmodule HomepageWeb.PageView do
   use HomepageWeb, :view
 
   def get_post_preview(%Homepage.Blog.Post{} = post) do
-    raw(Earmark.as_html!(post.content))
+    raw(
+      Earmark.as_html!(post.content, %Earmark.Options{
+        code_class_prefix: "lang-",
+        smartypants: false
+      })
+    )
   end
 end
